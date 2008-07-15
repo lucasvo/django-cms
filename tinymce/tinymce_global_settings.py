620a562add1bd788b1b13493826ac6a8a612adb8
divio <divio@92ec8317-e278-4cfe-9719-05471b91e6e2>
2008-07-15 12:18:48 +0000

    minor refactor of default settings
    git-svn-id: http://svn.django-cms.org/trunk@36 92ec8317-e278-4cfe-9719-05471b91e6e2

--- tinymce/tinymce_global_settings.py
+++ tinymce/tinymce_global_settings.py
@@ -21,6 +21,48 @@ default = dict(
     INVALID_ELEMENTS = "", # example: "font,strike,u"    
 )
 
+simple = dict(
+    MODE = 'exact',
+    PLUGINS = 'advimage,advlink,table,searchreplace,contextmenu,template,paste,save,autosave',
+    # Appearance
+    WIDTH = 480,
+    HEIGHT = 250,
+    THEME = 'advanced',
+    THEME_ADVANCED_RESIZING = True,
+    THEME_ADVANCED_PATH = True,
+    THEME_ADVANCED_BUTTONS1 = "formatselect,separator,pastetext,pasteword,separator,bold,italic,separator,bullist,numlist,separator,link,unlink,anchor,separator,image,separator,code",
+    THEME_ADVANCED_BUTTONS2 = "",
+    THEME_ADVANCED_BUTTONS3 = "",
+    THEME_ADVANCED_LAYOUT_MANAGER = "SimpleLayout",
+    # Styles
+    CONTENT_CSS = os.path.join(settings.MEDIA_URL, 'css/tinymce_style.css'),
+    SHOW_STYLES_MENU = True,
+    FORCED_ROOT_BLOCK = 'p',
+    EXTENDED_VALID_ELEMENTS = 'a[class|name|href|title|onclick],img[class|src|alt=image|title|onmouseover|onmouseout],p[id|style|dir|class],span[class|style]',
+    INVALID_ELEMENTS = "font,strike,u",    
+)
+
+spartanic = dict(
+    MODE = 'exact',
+    PLUGINS = 'advlink,contextmenu,template,paste,save,autosave',
+    # Appearance
+    WIDTH = 480,
+    HEIGHT = 250,
+    THEME = 'advanced',
+    THEME_ADVANCED_RESIZING = True,
+    THEME_ADVANCED_PATH = True,
+    THEME_ADVANCED_BUTTONS1 = "pastetext,pasteword,separator,bold,italic,separator,link,unlink,separator,code",
+    THEME_ADVANCED_BUTTONS2 = "",
+    THEME_ADVANCED_BUTTONS3 = "",
+    THEME_ADVANCED_LAYOUT_MANAGER = "SimpleLayout",
+    # Styles
+    CONTENT_CSS = os.path.join(settings.MEDIA_URL, 'css/tinymce_style.css'),
+    SHOW_STYLES_MENU = True,
+    FORCED_ROOT_BLOCK = 'p',
+    EXTENDED_VALID_ELEMENTS = 'a[class|name|href|title|onclick],img[class|src|alt=image|title|onmouseover|onmouseout],p[id|style|dir|class],span[class|style]',
+    INVALID_ELEMENTS = "font,strike,u",    
+)
+
 # Override the global settings with site-specific settings.
 try:
     from tinymce_settings import *
