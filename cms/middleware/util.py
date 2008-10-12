36c89c86afdfe8d8fef448061853877b97f2deab
jezdez <jezdez@92ec8317-e278-4cfe-9719-05471b91e6e2>
2008-10-12 13:54:24 +0000

    Cleaned up utility middlewares
    git-svn-id: http://svn.django-cms.org/trunk@62 92ec8317-e278-4cfe-9719-05471b91e6e2

--- cms/middleware/util.py
+++ cms/middleware/util.py
@@ -1,18 +1,18 @@
 import datetime
 import re
 
-re_ct_html = re.compile(r'^text/html\b')
+HTML_CONTENT_TYPE = re.compile(r'^text/html\b')
 
 class XHTMLToHTMLMiddleware:
     def process_response(self, request, response):
-        if re_ct_html.match(response['Content-Type']):
+        if HTML_CONTENT_TYPE.match(response['Content-Type']):
             response.content = response.content.replace(' />', '>')
         return response
 
 class BenchmarkMiddleware:
     def process_request(self, request):
         self.start = datetime.datetime.now()
-        print
+
     def process_response(self, request, response):
         if response.status_code == 200:
             stop = datetime.datetime.now()
