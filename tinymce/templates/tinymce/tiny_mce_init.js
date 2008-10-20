dd9ce6c2af749a42e157fe28b87a47db0602d401
divio <divio@92ec8317-e278-4cfe-9719-05471b91e6e2>
2008-10-20 12:11:46 +0000

    git-svn-id: http://svn.django-cms.org/trunk@77 92ec8317-e278-4cfe-9719-05471b91e6e2

--- tinymce/templates/tinymce/tiny_mce_init.js
+++ tinymce/templates/tinymce/tiny_mce_init.js
@@ -1,20 +1,22 @@
 {% autoescape off %}
 {% if file_browser %}
 function CustomFileBrowser(field_name, url, type, win) {
-    var fileBrowserWindow = new Array();
 
-    fileBrowserWindow['title'] = 'File Browser';
-    fileBrowserWindow['file'] = "/admin/filebrowser/?pop=2";
-    fileBrowserWindow['width'] = '820';
-    fileBrowserWindow['height'] = '600';
-    fileBrowserWindow['close_previous'] = 'no';
-    tinyMCE.openWindow(fileBrowserWindow, {
-      window : win,
-      input : field_name,
-      resizable : 'yes',
-      scrollbars : 'yes',
-      inline : 'yes',
-      editorID: tinyMCE.getWindowArg('editor_id')
+    var cmsURL = "/admin/filebrowser/?pop=2";
+    cmsURL = cmsURL + "&type=" + type;
+    
+    tinyMCE.activeEditor.windowManager.open({
+        file: cmsURL,
+        width: 820,  // Your dimensions may differ - toy around with them!
+        height: 500,
+        resizable: "yes",
+        scrollbars: "yes",
+        inline: "no",  // This parameter only has an effect if you use the inlinepopups plugin!
+        close_previous: "no",
+    }, {
+        window: win,
+        input: field_name,
+        editor_id: tinyMCE.selectedInstance.editorId,
     });
     return false;
 }
