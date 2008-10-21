f2bebb738606275a9806dd30e1a1da39f33ead5a
jezdez <jezdez@92ec8317-e278-4cfe-9719-05471b91e6e2>
2008-10-21 17:55:28 +0000

    Removed managers from models.py
    git-svn-id: http://svn.django-cms.org/branches/multisite@85 92ec8317-e278-4cfe-9719-05471b91e6e2

--- cms/models.py
+++ cms/models.py
@@ -13,11 +13,11 @@ from django.db.models import Q, get_app
 from django.utils import translation
 from django.utils.html import linebreaks, escape
 from django.contrib.sites.models import Site
-from django.contrib.sites.managers import CurrentSiteManager
 from django.contrib.markup.templatetags.markup import markdown, textile, \
                                                       restructuredtext as rst
 from cms.util import language_list, MetaTag
 from cms.conf.global_settings import LANGUAGE_REDIRECT, USE_TINYMCE, POSITIONS
+from cms.managers import PageManager, PageSiteManager
 
 # Look if django-tagging is installed, use its TagField and fall back to
 # Django's CharField if unavailable.
@@ -28,74 +28,6 @@ except ImproperlyConfigured:
     from django.models import CharField as TagField
 
 PROTOCOL_RE = re.compile('^\w+://')
-
-class RootPageDoesNotExist(Exception):
-    pass
-
-class PageManager(models.Manager):
-    def hierarchy(self, parent=None):
-        if parent:
-            pages = self.filter(parent=parent)
-        else:
-            try:
-                root = self.root()
-            except RootPageDoesNotExist:
-                return []
-            return [(root, self.hierarchy(root))]
-        return [(page, self.hierarchy(page)) for page in pages]
-
-    def root(self):
-        try:
-            return self.filter(parent__isnull=True)[0]
-        except IndexError:
-            raise RootPageDoesNotExist, unicode(_('Please create at least one page.'))
-
-    def published(self, user, now=datetime.datetime.now()):
-        queryset = self.all()
-        if not user.is_authenticated():
-            queryset = queryset.exclude(requires_login=True)
-        return queryset.filter(
-            Q(is_published=True),
-            Q(start_publish_date__lte=now) | Q(start_publish_date__isnull=True), 
-            Q(end_publish_date__gte=now) | Q(end_publish_date__isnull=True),
-        )
-
-    def in_navigation(self):
-        return self.filter(in_navigation=True)
-
-    def search(self, user, query, language=None):
-        queryset = self.published(user)
-        if language:
-            queryset = queryset.filter(
-                Q(title__icontains=query) |
-                Q(pagecontent__language=language) & (
-                    Q(pagecontent__title__icontains=query) |
-                    Q(pagecontent__description__icontains=query) |
-                    Q(pagecontent__content__icontains=query)
-                )
-            )
-        else:
-            queryset = queryset.filter(
-                Q(title__icontains=query) |
-                Q(pagecontent__title__icontains=query) |
-                Q(pagecontent__description__icontains=query) |
-                Q(pagecontent__content__icontains=query)
-            )
-        return queryset.distinct()
-
-    def get_by_overridden_url(self, user, url, raise404=True): # TODO: what is this for??
-        queryset = self.published(user)
-        try:
-            return queryset.get(overridden_url=url, override_url=True)
-        except AssertionError:
-            return queryset.filter(overridden_url=url, override_url=True)[0]
-        except Page.DoesNotExist:
-            if raise404:
-                raise Http404, unicode(_('Page does not exist. No page with overridden url "%s" was found.' % url))
-
-class PageSiteManager(PageManager, CurrentSiteManager):
-    pass
-
 
 class Page(models.Model):
     title = models.CharField(_('title'), max_length=200, help_text=_('The title of the page.'))
